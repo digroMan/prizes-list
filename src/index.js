557021d9962fe490a71c7a91fc822f213bfc8257
Dmitry Shirkov <dimashirkov01051995@gmail.com>
2025-01-27 16:44:32 +0300

    add swiper, animation and style

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './css/style.css';
+import './scss/helpers.scss'
+import './scss/mixin.scss';
+import './scss/fonts.scss';
+import './scss/style.scss'; 
+import 'swiper/scss';
+import 'swiper/scss/virtual';
+import 'swiper/scss/autoplay'
+import './js/app';
+
+import './scss/main.scss';
+
+// TODO: write your code in app.js
---
+++ src/js/app.js
@@ -0,0 +1,93 @@
+import { Swiper } from "swiper";
+import { Autoplay, Virtual } from "swiper/modules";
+import JSON_PRIZES_LIST from './prizes_list.json';
+
+// TODO: write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const BTTN_WHEEL_FORTUNE = document.querySelector('.wheel-fortune__button');
+  const SLIDES = document.querySelectorAll('.wheel-fortune__swiper-slide');
+
+  class WheelFortune {
+
+    prizesListStorage=[];
+    arrayAllId=[];
+
+    constructor (data){
+      this.prizesList = data.prizesListJson.prizes_list;
+      this.swiperWrapper = document.querySelector(`.${data.selectorSwiperWrapper}`);
+      this.selectorSwiperSlide = data.selectorSwiperSlide;
+      this.selectorSwiperImg = data.selectorSwiperImg; 
+    }
+
+    // Создает хранилище из элементов слайдера
+
+    createListStorage() {
+      this.prizesList.forEach(dataItem => {
+        this.prizesListStorage.push(this.createSlideItem(dataItem));
+        this.addIdItem(dataItem.id);
+      });
+    }
+
+    // Создает элемент слайда
+
+    createSlideItem(obj) {
+      const slideItem = document.createElement('div');
+      slideItem.classList.add('swiper-slide', this.selectorSwiperSlide);
+      slideItem.dataset.id = obj.id;
+      const slideImg = document.createElement('img');
+      slideImg.setAttribute("src", obj.img);
+      slideImg.classList.add(this.selectorSwiperImg);
+      slideItem.append(slideImg);
+      return slideItem;
+    }
+
+    // Добавляет слайды в слайдер
+
+    addSlidesInSlider() {
+      this.prizesListStorage.forEach(slide => this.swiperWrapper.append(slide));
+    }
+
+    // Добавляет ID в общий массив с ID
+
+    addIdItem(id) {this.arrayAllId.push(id)};
+  }
+
+  // Класс для создания елементов слайдера
+
+  const WHELL_FORTUNE = new WheelFortune(
+    {
+      prizesListJson: JSON_PRIZES_LIST,
+      selectorSwiperWrapper: 'wheel-fortune__swiper-wrapper',
+      selectorSwiperSlide: 'wheel-fortune__swiper-slide',
+      selectorSwiperImg: 'wheel-fortune__img',
+      winningId: '', // данное свойство должно содержать id товара, который выйграл пользователь
+    }
+  )
+
+  WHELL_FORTUNE.createListStorage();
+  WHELL_FORTUNE.addSlidesInSlider();
+
+  // Инициализация работы слайдера
+
+  const SWIPER_WHELL_FORTUNE = new Swiper(".wheel-fortune__swiper-container", {
+    modules: [Virtual, Autoplay],
+    breakpoints: {
+      320: {
+        direction: "vertical",
+        spaceBetween: 25,
+        slidesPerView: 4,
+        loop: true,
+        autoplay: {
+            disableOnInteraction: false,
+            delay: 0,
+        },
+        speed: 7000,
+        // allowTouchMove: false,
+      },
+    },
+  });
+
+  BTTN_WHEEL_FORTUNE.addEventListener("click", () => {
+    // SWIPER_WHELL_FORTUNE.slideTo(15, 17);
+  })
+});
